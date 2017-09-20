b351ba5c74d49bc1cd0574f09244b342e31a918a
HTK <tsekai.tk.hua@gmail.com>
2017-09-20 16:01:02 +0800

    End of Day 3

---
+++ CIY/index.js
@@ -0,0 +1,19 @@
+// // Start writing JS code
+
+// alert("Welcome to class")
+
+// // string
+// var student = "tsekai";
+
+
+// var i = 3.7
+// var t = typeof (i)
+
+var student_1 = "Tse";
+var student_2 = "Kai";
+
+var welcome_msg = "Hai. We are friends.." +  student_1 + " , " + student_2
+console.log(welcome_msg)
+
+
+
