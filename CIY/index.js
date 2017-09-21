e379ec95daf5b87ccd5048669d500d89fa09d12f
HTK <tsekai.tk.hua@gmail.com>
2017-09-21 13:46:17 +0800

    Day 4

--- CIY/index.js
+++ CIY/index.js
@@ -9,11 +9,32 @@
 // var i = 3.7
 // var t = typeof (i)
 
-var student_1 = "Tse";
-var student_2 = "Kai";
+// var student_1 = "Tse";
+// var student_2 = "Kai";
 
-var welcome_msg = "Hai. We are friends.." +  student_1 + " , " + student_2;
-console.log(welcome_msg)
+// var welcome_msg = "Hi. We are friends.." +  student_1 + " , " + student_2;
+// console.log(welcome_msg)
 
+// function greet(nameOfperson) {
+//     var greeting_message = "Welcome Mr" + nameOfperson;
+//     console.log(greeting_message)
+// }
 
+// greet("Bala");
+// greet("Tse");
+// greet("Kai");
+
+function name(a, b) {
+    var sum = a + b;
+    return sum;
+}
+
+var name1 = "Tse";
+var name2 = "Kai";
+
+var ans = name(name1, name2);
+console.log(ans);
+
+name1 ++;
+name2 ++;
 
